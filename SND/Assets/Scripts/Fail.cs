75b2046cf585d08f030f28ff05b25647251b43b6
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-11-09 11:07:05 +0900

    기능 추가
    스테이지 넘어가는 것 구현

---
+++ SND/Assets/Scripts/Fail.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fail : MonoBehaviour
+{   
+    public GameObject imageObject;
+
+    [SerializeField]
+    private float imageDisplayTime = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        imageObject.SetActive(false);
+    }
+
+
+    void OnMouseDown() {
+        StartCoroutine(DisplayImageAndProceed());
+    }
+
+    private IEnumerator DisplayImageAndProceed()
+    {
+        // 이미지 활성화
+        imageObject.SetActive(true);
+
+        // 지정된 시간 동안 대기
+        yield return new WaitForSeconds(imageDisplayTime);
+
+        // 이미지 비활성화
+        imageObject.SetActive(false);
+
+        // 다음 스테이지로 전환
+        LoadNextScene();
+    }
+
+    private void LoadNextScene()
+    {
+
+        SceneManager.LoadScene("SampleScene");
+        
+    }
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
